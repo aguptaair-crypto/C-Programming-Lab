44681b4a19b0912fc1388937503890f4ca13a416
aguptaair-crypto <aguptaair@gmail.com>
2025-10-22 15:14:28 +0530

    Create Introduction_About_Myself.c

---
+++ Lab-1/Introduction_About_Myself.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("MY NAME IS ANIKESH GUPTA \n");
+	printf("MY ROLL NO IS MEB25023 \n");
+	printf("MY DEPARTMENT IS MECHANICAL ENGINEERING \n");
+	printf("MY UNIVERSITY NAME IS TEZPUR UNIVERSITY");
+	return 0;
+}
